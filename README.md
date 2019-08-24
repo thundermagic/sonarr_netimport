bbb5e577710ac17c355319db5cb0f586770a830b
thundermagic <nitin.sidhu23@gmail.com>
2019-08-24 20:42:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# sonarr_netimport
+A sloppy code written in python to fetch TV series from TVDB and add it to sonarr.
+It can send email notification in case of an error with sync.
+
+Root folder for sonarr should be `/tv`.  
+All TV series are added to `any` sonarr profile.  
+If you want to change these two things, you can do it in the code, in the section
+```python
+sonarr_post_params = {
+                'addOptions': {'searchForMissingEpisodes': True},
+                'qualityProfileId': 1,  # Profile: any
+                'monitored': True,
+                'rootFolderPath': '/tv/'
+            }
+```
+
+#Config
+Script variables are passed as environment variables. Supported variables are;
+
+####Mandatory
+* TVDB_USERNAME: TVDB account username
+* TVDB_API_KEY: TVDB API key. You will be able to get this with your account
+* SONARR_IP: IP address where sonarr is listening
+* SONARR_PORT: Port number for sonarr. default is 8989
+* SONARR_API_KEY: API key for sonarr
+* SYNC_INTERVAL: Interval to which to sync tv series from TVBD with sonarr
+
+####Optional. These are for email notification
+* EMAIL_ADDRESS: Sender email address
+* EMAIL_TO_ADDRESS: Recipient email account 
+* EMAIL_PASSWORD: Sender email address password
+* SMTP_SERVER: smtp server 
+* SMTP_SERVER_PORT: Port number for smtp server
+
---
+++ sonarr_netimport.py
@@ -0,0 +1,138 @@
+import requests
+import smtplib
+import os
+import traceback
+from time import sleep
+from email.message import EmailMessage
+
+
+def send_email(message):
+    """ Sends email """
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Sonarr failure'
+    msg['From'] = email_address
+    msg['To'] = email_to_address
+
+    msg.set_content('Sonarr netimport list failure\n\n'
+                    'Error msg: {0}'.format(message))
+
+    with smtplib.SMTP(os.environ.get('SMTP_SERVER'), int(os.environ.get('SMTP_SERVER_PORT'))) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+    return None
+
+
+def get_tvdb_jwt():
+
+    login_post = {"apikey": tvdb_api_key, "username": tvdb_username, "userkey": tvdb_user_key}
+    login_url = 'https://api.thetvdb.com/login'
+    jwt = requests.post(login_url, json=login_post)
+    return jwt.json()['token']
+
+
+def get_tvdb_show_ids() -> set:
+    """ Returns tvdbId of shows in TVDB list """
+
+    jwt = get_tvdb_jwt()
+    auth = {'Authorization': 'Bearer {0}'.format(jwt)}
+    shows = requests.get('https://api.thetvdb.com/user/favorites', headers=auth)
+    show_ids = shows.json()['data']['favorites']
+    return set([int(show_id) for show_id in show_ids])
+
+
+def get_show_ids_from_sonarr() -> set:
+    """ Return tvdbId of shows already in sonarr """
+
+    shows = requests.get(sonarr_url, headers=sonarr_headers).json()
+    show_ids = [show['tvdbId'] for show in shows]
+    return set(show_ids)
+
+
+def add_shows_sonarr(tvdb_shows: set) -> dict:
+    result = {}
+    for show_id in tvdb_shows:
+        sonarr_post_params['tvdbId'] = show_id
+        resp = requests.post(sonarr_url, headers=sonarr_headers, json=sonarr_post_params)
+        result[show_id] = resp.status_code
+    return result
+
+
+if __name__ == '__main__':
+
+    tvdb_username = os.environ.get('TVDB_USERNAME')
+    tvdb_user_key = os.environ.get('TVDB_USER_KEY')
+    tvdb_api_key = os.environ.get('TVDB_API_KEY')
+
+    sonarr_ip = os.environ.get('SONARR_IP')
+    sonarr_port = os.environ.get('SONARR_PORT')
+    sonarr_api_key = os.environ.get('SONARR_API_KEY')
+
+    email_address = os.environ.get('EMAIL_ADDRESS')
+    email_to_address = os.environ.get('EMAIL_TO_ADDRESS')
+    email_password = os.environ.get('EMAIL_PASSWORD')
+    smtp_server = os.environ.get('SMTP_SERVER')
+    smtp_server_port = os.environ.get('SMTP_SERVER_PORT')
+
+    while True:
+        try:
+            print('Sleeping for one hour')
+            print()
+            sleep(int(os.environ.get('SYNC_INTERVAL')))
+
+            sonarr_headers = {
+                'X-API-KEY': sonarr_api_key
+            }
+            sonarr_url = 'http://{ip}:{port}/api/series'.format(ip=sonarr_ip, port=sonarr_port)
+
+            sonarr_post_params = {
+                'addOptions': {'searchForMissingEpisodes': True},
+                'qualityProfileId': 1,  # Profile: any
+                'monitored': True,
+                'rootFolderPath': '/tv/'
+            }
+
+            print('Getting shows from TVDB')
+            tvdb_shows = get_tvdb_show_ids()
+            print('Shows in TVDB list: {0}'.format(tvdb_shows))
+
+            print('Getting shows from sonarr')
+            sonarr_shows = get_show_ids_from_sonarr()
+            print('Shows in sonarr: {0}'.format(sonarr_shows))
+
+            shows_to_add = tvdb_shows.difference(sonarr_shows)
+            if shows_to_add:
+                print('Shows to add to sonarr: {0}'.format(shows_to_add))
+                print('Adding shows to sonarr')
+                add_result = add_shows_sonarr(shows_to_add)
+                print('Show add result: {0}'.format(add_result))
+
+                failed = {}
+                for show_id, status_code in add_result.items():
+                    if status_code != 201:
+                        failed[show_id] = status_code
+
+                if failed:
+                    if email_address is None \
+                            or email_password is None \
+                            or smtp_server is None \
+                            or smtp_server_port is None:
+                        pass
+                    else:
+                        print('sending email notification')
+                        send_email('Show failed to be added: {0}'.format(failed))
+            else:
+                print('No shows to add to sonarr')
+        except Exception as err:
+            print('Unexpected error')
+            print(traceback.format_exc())
+            if email_address is None \
+                    or email_password is None \
+                    or smtp_server is None \
+                    or smtp_server_port is None:
+                pass
+            else:
+                print('sending email notification')
+                send_email(str(traceback.format_exc()))
